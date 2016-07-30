ed82461df86f15b69b74252ccc43e16db8fd2c8f
Marc Ruiz - radykal - <radykal@radykal.com>
2016-07-30 22:05:51 +0200

    Check if stack already exists before trying to create it Throw errors to stderr Show info messages of script execution

--- aws/stack.py
+++ aws/stack.py
@@ -2,3 +2,12 @@ def create_stack_name(application, environment):
     separator = '-'
     name = separator.join([application, environment])
     return name
+
+
+def stack_exists(stack_name, aws_session):
+    cf = aws_session.client('cloudformation')
+    stacks = cf.describe_stacks().get('Stacks')
+    for stack in stacks:
+        if stack.get('StackName') == stack_name:
+            return True
+    return False
--- create-stack.py
+++ create-stack.py
@@ -9,31 +9,39 @@ TEMPLATE_DIR = "templates"
 TEMPLATE_EXT = ".json"
 AWS_REGION = "eu-west-1"
 
-argumentsManager = ArgumentsManager(parameters.autoParams)
-arguments = argumentsManager.parse_args()
-applicationName = arguments.Application
-environment = arguments.Environment
-
-templateFileName = applicationName + TEMPLATE_EXT
-templateFilePath = os.path.sep.join([ROOT_PATH, TEMPLATE_DIR,templateFileName])
-
-try:
-    with open(templateFilePath, 'r') as templateFile:
-        templateContent = templateFile.read()
-except:
-    print("Unknown application (template file not found): " + applicationName)
-    sys.exit(1)
-
 session = boto3.session.Session(
     region_name=AWS_REGION
 )
 
+argumentsManager = ArgumentsManager(parameters.autoParams)
+arguments = argumentsManager.parse_args()
+application = arguments.Application
+environment = arguments.Environment
+
+templateFileName = application + TEMPLATE_EXT
+templateFilePath = os.path.sep.join([ROOT_PATH, TEMPLATE_DIR,templateFileName])
+try:
+    with open(templateFilePath, 'r') as templateFile:
+        templateContent = templateFile.read()
+        print("Found template file for application: " + application)
+except:
+    sys.stderr.write("Error: Unknown application (template file not found): " + application)
+    sys.exit(1)
+
+stackName = stack.create_stack_name(application, environment)
+if stack.stack_exists(stackName, session):
+    sys.stderr.write("Error: Stack with name " + stackName + " already exists")
+    sys.exit(1)
+
 cfClient = session.client('cloudformation')
 response = cfClient.validate_template(TemplateBody=templateContent)
+print("Stack template validated")
+
+print("Passing cli-arguments as stack parameters")
 argumentsManager.add_parameters_as_arguments(response.get('Parameters', None))
 args = vars(argumentsManager.get_arguments())
 autoParams = parameters.set_auto_params_values(
-    applicationName,
+    application,
     environment,
     argumentsManager.get_auto_params(),
     session
@@ -41,8 +49,13 @@ autoParams = parameters.set_auto_params_values(
 argParams = parameters.parse_arguments_as_parameters(args)
 parameters = autoParams+argParams
 
+print("Creating stack with name: " + stackName)
 cfClient.create_stack(
-    StackName=stack.create_stack_name(application=applicationName, environment=environment),
+    StackName=stackName,
     TemplateBody=templateContent,
     Parameters=parameters
 )
+print("Stack creation running...waiting for completion")
+waiter = cfClient.get_waiter('stack_create_complete')
+waiter.wait(StackName=stackName)
+print("Stack creation finished, checking stack status")
